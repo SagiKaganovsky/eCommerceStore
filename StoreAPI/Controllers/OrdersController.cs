40a7d6886fc7f628ae7e12fab556544cc2536b54
Sagi Kaganovsky <sergkagan@gmail.com>
2023-01-08 18:46:55 +0200

    created orders controller

---
+++ StoreAPI/Controllers/OrdersController.cs
@@ -0,0 +1,37 @@
+﻿using Micrhosoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreAPI.Data;
+using StoreAPI.Entities.OrderAggregate;
+
+namespace StoreAPI.Controllers
+{
+    [Authorize]
+    public class OrdersController : BaseApiController
+    {
+        private readonly StoreContext _storeContext;
+
+        public OrdersController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Order>>> GetOrders()
+        {
+            return await _storeContext.Orders
+                .Include(o => o.OrderItems)
+                .Where(u=> u.BuyerId == User.Identity.Name).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            return await _storeContext.Orders
+                .Include(o => o.OrderItems)
+                .Where( u => u.BuyerId == User.Identity.Name && u.Id == id).FirstOrDefaultAsync();
+                
+        }
+    }
+}
